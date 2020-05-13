ad162691e8fd276033ae18730cb1929d50295a42
veca22 <57525487+veca22@users.noreply.github.com>
2020-05-13 16:06:22 +0200

    Agentski backend

---
+++ Agentski-backend/src/test/java/com/agent/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
