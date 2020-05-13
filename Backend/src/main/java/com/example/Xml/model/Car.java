29a469144d7d5fef16de0ee0abdf23347e331fad
nikzejak <nik.zejak@gmail.com>
2020-05-13 12:51:54 +0200

    dodatna polja za auto

--- Backend/src/main/java/com/example/Xml/model/Car.java
+++ Backend/src/main/java/com/example/Xml/model/Car.java
@@ -3,6 +3,10 @@ package com.example.Xml.model;
 import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table
@@ -34,12 +38,131 @@ public class Car {
     private float mileage;
 
     @Column(nullable = false)
+    private CarStatus carStatus;
+
+    @Column(nullable = false)
     private float distanceAllowed;
-
-
 
     @Column(nullable = false)
     @Range(min = 0, max = 5)
     private int childSeats;
 
+    @Column(nullable = false)
+    private double averageRating = 0;
+
+    //private ArrayList<String> comments = new ArrayList<String>();
+
+    public Car(Long id, CarBrand carBrand, CarType carType, String fuelType, String transmissionType, @Range(min = 0, max = 1000000) int price, @Range(min = 0, max = 1000000) float mileage, CarStatus carStatus, float distanceAllowed, @Range(min = 0, max = 5) int childSeats, double averageRating, ArrayList<String> comments) {
+        this.id = id;
+        this.carBrand = carBrand;
+        this.carType = carType;
+        this.fuelType = fuelType;
+        this.transmissionType = transmissionType;
+        this.price = price;
+        this.mileage = mileage;
+        this.carStatus = carStatus;
+        this.distanceAllowed = distanceAllowed;
+        this.childSeats = childSeats;
+        this.averageRating = averageRating;
+        //this.comments = comments;
+        //this.comments = new ArrayList<String>();
+    }
+
+    public Car() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CarBrand getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(CarBrand carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public float getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(float mileage) {
+        this.mileage = mileage;
+    }
+
+    public CarStatus getCarStatus() {
+        return carStatus;
+    }
+
+    public void setCarStatus(CarStatus carStatus) {
+        this.carStatus = carStatus;
+    }
+
+    public float getDistanceAllowed() {
+        return distanceAllowed;
+    }
+
+    public void setDistanceAllowed(float distanceAllowed) {
+        this.distanceAllowed = distanceAllowed;
+    }
+
+    public int getChildSeats() {
+        return childSeats;
+    }
+
+    public void setChildSeats(int childSeats) {
+        this.childSeats = childSeats;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+
+//    public ArrayList<String> getComments() {
+//        return comments;
+//    }
+//
+//    public void setComments(ArrayList<String> comments) {
+//        this.comments = comments;
+//    }
 }
