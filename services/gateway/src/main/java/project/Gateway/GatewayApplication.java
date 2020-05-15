6b6949af97f965571c4baf5bdca1b8fc8cb901a0
veca22 <57525487+veca22@users.noreply.github.com>
2020-05-15 16:47:49 +0200

    Gateway

---
+++ services/gateway/src/main/java/project/Gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package project.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GatewayApplication {
+
+	@RequestMapping("/health")
+	public String home() {
+		return "Hello from gateway";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
