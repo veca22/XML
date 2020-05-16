9b19a893258257de61086e700e58b3a4f13fb2a1
mirkela11 <57531634+mirkela11@users.noreply.github.com>
2020-05-16 11:59:59 +0200

    AdService

---
+++ services/adService/src/test/java/service/AdService/AdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package service.AdService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
