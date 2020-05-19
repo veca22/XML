5b1dd7b11932b014c511e912531b2692ea7d2c27
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-05-19 14:29:50 +0200

    update renting

--- services/rentingService/src/main/java/service/rentingService/RentingServiceApplication.java
+++ services/rentingService/src/main/java/service/rentingService/RentingServiceApplication.java
@@ -3,8 +3,10 @@ package service.rentingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RentingServiceApplication {
 
 	@RequestMapping("/health")
