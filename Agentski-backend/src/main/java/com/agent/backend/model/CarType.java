6a9790e0da24df92c79c39a055324878c84d689f
Stefan Dragojevic <dragojevicstefan98@gmail.com>
2020-05-20 14:20:10 +0200

    agentski backend

---
+++ Agentski-backend/src/main/java/com/agent/backend/model/CarType.java
@@ -0,0 +1,9 @@
+package com.agent.backend.model;
+
+public enum CarType {
+    SUV,
+    OLD_TIMER,
+    CITY_CAR,
+    MINIVAN,
+    WAGON
+}
---
+++ Agentski-backend/src/main/java/com/agent/backend/model/RentRequest.java
@@ -0,0 +1,78 @@
+package com.agent.backend.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+public class RentRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime reservedFrom;
+
+    @Column(nullable = false)
+    private LocalDateTime reservedTo;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RentRequestStatus rentRequest;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Car> carsForRent;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getReservedFrom() {
+        return reservedFrom;
+    }
+
+    public void setReservedFrom(LocalDateTime reservedFrom) {
+        this.reservedFrom = reservedFrom;
+    }
+
+    public LocalDateTime getReservedTo() {
+        return reservedTo;
+    }
+
+    public void setReservedTo(LocalDateTime reservedTo) {
+        this.reservedTo = reservedTo;
+    }
+
+    public RentRequestStatus getRentRequest() {
+        return rentRequest;
+    }
+
+    public void setRentRequest(RentRequestStatus rentRequest) {
+        this.rentRequest = rentRequest;
+    }
+
+    public Set<Car> getCarsForRent() {
+        return carsForRent;
+    }
+
+    public void setCarsForRent(Set<Car> carsForRent) {
+        this.carsForRent = carsForRent;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
