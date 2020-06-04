0c159a9622617ebca7ed53afbe1ad5a178371870
veca22 <57525487+veca22@users.noreply.github.com>
2020-06-04 16:38:03 +0200

    update

--- Frontend/src/app/components/ad-view-dialog/ad-view-dialog.component.ts
+++ Frontend/src/app/components/ad-view-dialog/ad-view-dialog.component.ts
@@ -10,11 +10,19 @@ import {Ad} from '../../model/ad';
 })
 export class AdViewDialogComponent implements OnInit {
 
+  images: Array<string> = new Array<string>();
+  image: string;
+  i = 0;
   ad: Ad;
   constructor(public dialogRef: MatDialogRef<AdViewDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
               public adService: AdService,
-              public dialog: MatDialog) { }
+              public dialog: MatDialog) {
+
+    this.images.push('https://material.angular.io/assets/img/examples/shiba2.jpg');
+    this.images.push('https://grupovina.rs/upload/iblock/d0b/d0b21670bf15ff256a4db044d1bccbfc.jpg');
+    this.image = this.images[0];
+  }
 
   ngOnInit() {
     this.ad = this.data;
@@ -23,6 +31,22 @@ export class AdViewDialogComponent implements OnInit {
 
   close() {
     this.dialogRef.close();
+  }
+
+  nextImage() {
+    if (this.i < this.images.length - 1) {
+      this.i = this.i + 1;
+    }
+
+    this.image = this.images[this.i];
+
+  }
+
+  previous_image() {
+    if (this.i > 0) {
+      this.i = this.i - 1;
+    }
+    this.image = this.images[this.i];
   }
 
 }
