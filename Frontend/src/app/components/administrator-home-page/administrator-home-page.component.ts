0c55371a8dfcaa226aa8198db20b984894982d2d
mirkela11 <57531634+mirkela11@users.noreply.github.com>
2020-06-05 20:46:30 +0200

    finally fix

--- Frontend/src/app/components/administrator-home-page/administrator-home-page.component.ts
+++ Frontend/src/app/components/administrator-home-page/administrator-home-page.component.ts
@@ -5,6 +5,12 @@ import {CarBrand} from "../../model/carBrand";
 import {CarBrandService} from "../../services/car-brand.service";
 import {CarModel} from "../../model/carModel";
 import {CarModelService} from "../../services/car-model.service";
+import {CarType} from '../../model/carType';
+import {FuelType} from '../../model/fuelType';
+import {TransmissionType} from '../../model/transmissionType';
+import {CarTypeService} from '../../services/car-type.service';
+import {FuelTypeService} from '../../services/fuel-type.service';
+import {TransmissionTypeService} from '../../services/transmission-type.service';
 
 @Component({
   selector: 'app-administrator-home-page',
@@ -15,12 +21,19 @@ export class AdministratorHomePageComponent implements OnInit {
   tmp: Array<User>;
   b: Array<CarBrand>;
   m: Array<CarModel>;
+  t: Array<CarType>;
+  f: Array<FuelType>;
+  trans: Array<TransmissionType>;
 
-  constructor(private userService: UserService, private carBrandService: CarBrandService,
+  constructor(private userService: UserService, private carBrandService: CarBrandService, private carTypeSerivice: CarTypeService, private fuelTypeService: FuelTypeService,
+              private transmissionTypeService: TransmissionTypeService,
               private carModelService: CarModelService) {
     this.tmp = this.userService.getEndUsersForOperations();
     this.b = this.carBrandService.getAllCarBrand();
     this.m = this.carModelService.getAllCarModel();
+    this.t = this.carTypeSerivice.getAllCarType();
+    this.f = this.fuelTypeService.getAllFuelType();
+    this.trans = this.transmissionTypeService.getAllTransmissionType();
   }
 
   ngOnInit() {
--- services/adService/src/main/resources/data-postgres.sql
+++ services/adService/src/main/resources/data-postgres.sql
@@ -1,22 +1,6 @@
 
 insert into users(id, email, password, role, status) values (115,'testic@test.com','test','ENDUSER', 'ACCEPTED');
-insert into fuel_type(id, serial_number, type) values(277, '123b86', 'jeriDizel');
-insert into fuel_type(id, serial_number, type) values(300, '253ac', 'benzin');
-insert into fuel_type(id, serial_number, type) values(305, '152ca', 'plin');
 
-insert into transmission_type(id, serial_number, type) values(222, 'km32', 'automatik');
-
-insert into car_type(id, type) values (333, 'city_car');
-insert into car_type(id, type) values (334, 'minivan');
-insert into car_type(id, type) values (335, 'wagon');
-
-insert into car_brand(id, brand) values (444, 'VW');
-insert into car_brand(id, brand) values (445, 'Audi');
-insert into car_brand(id, brand) values (446, 'Rolls Royce');
-
-insert into car_model(id, model) values (555, 'A6 2016');
-insert into car_model(id, model) values (556, 'Insignia 2020');
-insert into car_model(id, model) values (557, 'S500 2014');
 insert into car(id,average_rating,car_status,car_type_id,child_seats,collision_damage_waiver,discount,distance_allowed,fuel_type_id,mileage,price,transmission_type_id,car_model_id,car_brand_id) values (100,0,'RENTED',333,0,'false',10,100,277,60000,40,222,555,444);
 insert into client(id,ad_counter,address,email,first_name,last_name,jmbg,password,phone_number,role) values (100,0,'Radnicka 36','steva@email.com','Stefan','Dragojevic','1601998772017','steva','12345798','ENDUSER');
 insert into client(id,ad_counter,address,email,first_name,last_name,jmbg,password,phone_number,role) values (101,0,'Gazdina','gazda@email.com','Mirkela','Gazda','111111111','gazda','12345744','ADMINISTRATOR');
