41fa7f4686c0efdb3a98c2bf3bf9ceede103401c
mirkela11 <57531634+mirkela11@users.noreply.github.com>
2020-06-05 13:44:06 +0200

    uradjen izgled celog oglasa

--- Frontend/src/app/components/ad-view-dialog/ad-view-dialog.component.ts
+++ Frontend/src/app/components/ad-view-dialog/ad-view-dialog.component.ts
@@ -1,9 +1,10 @@
 import {Component, HostListener, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef, MatTableDataSource} from '@angular/material';
 import {AdService} from '../../services/ad.service';
 import {Ad} from '../../model/ad';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Route, Router} from '@angular/router';
+import {DataSource} from '@angular/cdk/table';
 
 @Component({
   selector: 'app-ad-view-dialog',
@@ -16,19 +17,19 @@ export class AdViewDialogComponent implements OnInit {
   image: string;
   i = 0;
   ad: Ad;
-  selectedAd: Ad;
   submitted = false;
   adViewForm: FormGroup;
+
 
   constructor(private formBuilder: FormBuilder, private router: Router, private adService: AdService, public dialogRef: MatDialogRef<AdViewDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
               public dialog: MatDialog) {
 
-    this.images.push("assets/images/1.jpg");
-    this.images.push("assets/images/2.jpg");
-    this.images.push("assets/images/3.jpg");
-    this.images.push("assets/images/4.jpg");
-    this.images.push("assets/images/5.jpg");
+    this.images.push('assets/images/1.jpg');
+    this.images.push('assets/images/2.jpg');
+    this.images.push('assets/images/3.jpg');
+    this.images.push('assets/images/4.jpg');
+    this.images.push('assets/images/5.jpg');
 
     this.image = this.images[0];
 
@@ -38,13 +39,22 @@ export class AdViewDialogComponent implements OnInit {
     this.ad = this.data;
     console.log(this.ad);
 
-      this.adViewForm = this.formBuilder.group({
+    this.adViewForm = this.formBuilder.group({
       title: new FormControl(this.data.title),
       description: new FormControl(this.data.description),
       carBrand: new FormControl(this.data.car.carBrand.brand),
       carModel: new FormControl(this.data.car.carModel.model),
       carType: new FormControl(this.data.car.carType.type),
-      fuelType: new FormControl(this.data.car.fuelType.type)
+      fuelType: new FormControl(this.data.car.fuelType.type),
+      transmissionType: new FormControl(this.data.car.transmissionType.type),
+      distanceAllowed: new FormControl(this.data.car.distanceAllowed),
+      mileage: new FormControl(this.data.car.mileage),
+      place: new FormControl(this.data.place),
+      price: new FormControl(this.data.car.price),
+      carStatus: new FormControl(this.data.car.carStatus),
+      childSeats: new FormControl(this.data.car.childSeats)
+
+
     });
   }
 
