afc85e3195f49533c45c2af159a4f4d2c4a93575
nikzejak <nik.zejak@gmail.com>
2020-06-19 15:43:01 +0200

    web_config_and_endpoint

---
+++ Agentski-backend/src/main/java/com/agent/backend/endpoint/EndPoint.java
@@ -0,0 +1,9 @@
+package com.agent.backend.endpoint;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
+public class EndPoint {
+
+    private static final String NAMESPACE = "http://spring.io/guides/gs-producing-web-service";
+}
